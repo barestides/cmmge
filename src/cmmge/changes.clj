db8dea0bc90df116db592ed4e03389d960225f27
Braden Arestides <braden@arestides.com>
2018-01-01 16:14:24 -0500

    Adds changes ns.

---
+++ src/cmmge/changes.clj
@@ -0,0 +1,24 @@
+(ns cmmge.changes)
+
+
+;;We can think of music as series of changes over time.
+;;These can be "fine" changes within a melody, or with a larger contect, changes to song
+;;structure for example.
+
+;;more thoughts:,
+
+;;If a sound has too many changes, the listener cannot keep track of what is happening,
+;;and even if it's organized, if the listener can't organize it, subconsiously or consiously,
+;;it will just sound like noise. But if a sound doesn't have enough changes, it sound simple,
+;;and isn't engaging to listen to.
+
+
+;;Is popular music dull because the songs themselves are simple, or are they more formulaic,
+;;so people have a better idea of what to expect?
+
+;;Things that affect complexity:
+;; instrument count
+;; syncopation
+;; melodic / harmonic divergence from the key
+;;like with modes, if you are in the key of c major, throwing in an f# isn't as
+;;much of a divergence as a d#
